96587d6106d36afbedafed89e2aaff8ea9edb908
WaqasBashir86 <74374255+WaqasBashir86@users.noreply.github.com>
2021-02-21 21:30:22 -0500

    adding Nunit in to core project and test code

--- WB_Assignment_01/WB_Assignment_01_Test/Program.cs
+++ WB_Assignment_01/WB_Assignment_01_Test/Program.cs
@@ -10,11 +10,139 @@ using NUnit.Framework;
 
 namespace WB_Assignment_01_Test
 {
-    class Program
-    {
-        static void Main(string[] args)
+  
+      
+        [TestFixture]
+        public class Class1
         {
-            Console.WriteLine("Hello World!");
+            Rectangle r1 = new Rectangle(3, 5);
+
+            [Test]
+            public void GetLengthTest1()
+            {
+                int expected = 3;
+
+                int res = r1.GetLength();
+
+                Assert.AreEqual(expected, res, 1);
+            }
+
+            [Test]
+            public void GetLengthTest2()
+            {
+                int expected = 4;
+
+                int res = r1.GetLength();
+
+                Assert.AreNotEqual(expected, res);
+            }
+
+
+            [Test]
+            public void SetLengthTest1()
+            {
+                int expected = 4;
+
+                int res = r1.SetLength(4);
+
+                Assert.AreEqual(expected, res);
+            }
+
+
+
+            [Test]
+            public void SetLengthTest2()
+            {
+                int expected = 4;
+
+                int res = r1.SetLength(6);
+
+                Assert.AreNotEqual(expected, res);
+            }
+
+
+            [Test]
+            public void GetWidthTest1()
+            {
+                int expected = 5;
+
+                int res = r1.GetWidth();
+
+                Assert.AreEqual(expected, res);
+            }
+
+            [Test]
+            public void GetWidthTest2()
+            {
+                int expected = 4;
+
+                int res = r1.GetWidth();
+
+                Assert.AreNotEqual(expected, res);
+            }
+
+
+            [Test]
+            public void SetWidthTest1()
+            {
+                int expected = 4;
+
+                int res = r1.SetWidth(4);
+
+                Assert.AreEqual(expected, res);
+            }
+
+            [Test]
+            public void SetWidthTest2()
+            {
+                int expected = 4;
+
+                int res = r1.SetWidth(3);
+
+                Assert.AreNotEqual(expected, res);
+            }
+
+            [Test]
+            public void Perimeter1()
+            {
+                int expected = 16;
+
+                int res = r1.GetPerimeter();
+
+                Assert.AreEqual(expected, res);
+            }
+
+            [Test]
+            public void Perimeter2()
+            {
+                int expected = 4;
+
+                int res = r1.GetPerimeter();
+
+                Assert.AreNotEqual(expected, res);
+            }
+
+
+            [Test]
+            public void Area1()
+            {
+                int expected = 15;
+
+                int res = r1.GetArea();
+
+                Assert.AreEqual(expected, res);
+            }
+
+            [Test]
+            public void Area2()
+            {
+                int expected = 4;
+
+                int res = r1.GetArea();
+
+                Assert.AreNotEqual(expected, res);
+            }
         }
     }
+
 }
