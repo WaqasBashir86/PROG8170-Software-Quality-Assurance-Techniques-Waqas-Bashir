6a5d91181ec2261b59301940ad089a3675e97d0c
WaqasBashir86 <74374255+WaqasBashir86@users.noreply.github.com>
2021-02-21 21:46:35 -0500

    last NUNIt addition did not seem to work. I am adding a new test project to add references and run NUNIT GUI

---
+++ WB_Assignment_01/A1_Test/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.framework;
+using WB_Assignment_01;
+
+namespace A1_Test
+{
+    public class Class1
+    {
+    }
+}
