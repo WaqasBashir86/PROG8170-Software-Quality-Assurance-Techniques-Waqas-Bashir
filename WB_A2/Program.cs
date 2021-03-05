74cf122e7257f9d9b0e682246ca138be11872f1c
WaqasBashir86 <74374255+WaqasBashir86@users.noreply.github.com>
2021-03-05 15:07:50 -0500

    Project build up and class creation for Triangle solver

--- WB_A2/Program.cs
+++ WB_A2/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace WB_A2
 {
@@ -6,7 +10,82 @@ namespace WB_A2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char answer;
+            do
+            {
+                Console.WriteLine(" Welcome to Triangle Solver Select a menu option to continue \n");
+                Console.WriteLine("1.Enter triangle dimension");
+                Console.WriteLine("2.Exit \n");
+
+                Console.WriteLine("Enter your menu choice \n");
+
+                TriangleSolver tri = new TriangleSolver();
+                var selection = Console.ReadLine();
+
+                if (isNumber(selection))
+                {
+                    String result = "";
+
+                    if (Convert.ToInt32(selection) == 1 || Convert.ToInt32(selection) == 2)
+                    {
+                        switch (selection)
+                        {
+
+                            case "1":
+                                Console.WriteLine("Enter triangle dimension ");
+                                break;
+
+                            case "2":
+                                return;
+                        }
+
+                        TriangleSolver tro = new TriangleSolver();
+
+                        Console.Write("Input side 1 of triangle: ");
+                        var a = Console.ReadLine();
+                        Console.Write("Input side 2 of triangle: ");
+                        var b = Console.ReadLine();
+                        Console.Write("Input side 3 of triangle: ");
+                        var c = Console.ReadLine();
+
+
+                        if (isNumber(a) && isNumber(b) && isNumber(c))
+                        {
+                            result = TriangleSolver.Analyze(Convert.ToInt32(a), Convert.ToInt32(b), Convert.ToInt32(c));
+                        }
+
+
+                        Console.WriteLine(result);
+
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("Enter valid menu option \n");
+
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Entered character is not a number");
+                }
+
+                Console.ReadLine();
+                Console.WriteLine("Do you want to continue");
+                answer = Convert.ToChar(Console.ReadLine());
+
+            } while (answer == 'y' || answer == 'Y');
+
+        }
+        public static bool isNumber(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+                if (char.IsDigit(s[i]) == false)
+                    return false;
+
+            return true;
+
         }
     }
 }
--- WB_A2/TriangleSolver.cs
+++ WB_A2/TriangleSolver.cs
@@ -1,10 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace WB_A2
 {
     class TriangleSolver
     {
+
+        public static String Analyze(int a, int b, int c)
+        {
+            TriangleSolver tr = new TriangleSolver();
+
+
+            if (tr.checkValidity(Convert.ToInt32(a), Convert.ToInt32(b), Convert.ToInt32(c)))
+            {
+                String triangleName = tr.whichTriangle(Convert.ToInt32(a), Convert.ToInt32(b), Convert.ToInt32(c));
+
+                return triangleName;
+
+            }
+            else
+            {
+                return "Not form a triangle";
+
+            }
+
+        }
+
+
+
+        public String whichTriangle(int a, int b, int c)
+        {
+            if (a == b && b == c && c == a)
+            {
+                return "It is an Equilateral Triangle";
+
+            }
+            else if (a == b || a == c || b == c)
+            {
+                return "It is an Isosceles Triangle";
+
+
+            }
+            else if (a != b && b != c && c != a)
+            {
+                return "It is a Scalene Triangle";
+
+            }
+
+            return "Triangle Not Found";
+
+        }
+
+        public bool checkValidity(int a, int b, int c)
+        {
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
     }
 }
