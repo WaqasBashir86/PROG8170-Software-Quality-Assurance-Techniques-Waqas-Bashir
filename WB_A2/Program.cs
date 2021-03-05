de99000cd69c1519f90cec703d868e493fe5b3fe
WaqasBashir86 <74374255+WaqasBashir86@users.noreply.github.com>
2021-03-05 15:01:27 -0500

    Project creation and TriangleSolver class addition

---
+++ WB_A2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WB_A2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WB_A2/TriangleSolver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WB_A2
+{
+    class TriangleSolver
+    {
+    }
+}
